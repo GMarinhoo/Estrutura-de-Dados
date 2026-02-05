38f807113bd4ba0839e287941f9b50a651c49fe7
Gabriel Marinho Bezerra <gabrielmarinhobezerra@gmail.com.br>
2026-02-05 20:16:59 -0300

    Tipo Primitivo

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,26 @@ public class Main {
     public static void main(String[] args) {
         // Tipo Inteiro
         byte menor = 19;
-        System.out.println(menor);
+        System.out.println("Byte: " + menor);
         short medio = 1920;
+        System.out.println("Short: " + medio);
         int medio2 = 192021;
-        System.out.println(medio);
+        System.out.println("Int: " + medio);
         long grande = 19202122L;
-        System.out.println(grande);
+        System.out.println("Long: " + grande);
 
         //Ponto Flutuante
         float flutu = 3.14F;
-        System.out.println(flutu);
+        System.out.println("Float: " + flutu);
         double ante = 3.14159;
-        System.out.println(ante);
+        System.out.println("Double: " + ante);
 
         //Caracter
         char um = 'M';
+        System.out.println("Char: " + um);
 
         //Boolean
         boolean idade = menor >= 18;
-        System.out.println(idade);
+        System.out.println("Boolean: " + idade);
     }
 }
