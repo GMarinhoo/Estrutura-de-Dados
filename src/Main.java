a3a48d35ea8160a493c69cef6138a2f51ea47e77
Gabriel Marinho Bezerra <gabrielmarinhobezerra@gmail.com.br>
2026-02-05 19:58:21 -0300

    Tipo Primitivo

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipo Inteiro
+        byte menor = 19;
+        System.out.println(menor);
+        short medio = 1920;
+        int medio2 = 192021;
+        System.out.println(medio);
+        long grande = 19202122L;
+        System.out.println(grande);
+
+        //Ponto Flutuante
+        float flutu = 3.14F;
+        System.out.println(flutu);
+        double ante = 3.14159;
+        System.out.println(ante);
+
+        //Caracter
+        char um = 'M';
+
+        //Boolean
+        boolean idade = menor >= 18;
+        System.out.println(idade);
+    }
+}
